52a94d45e38b3f1603b4499884d068207cc66294
rknm-cell <racknahm@gmail.com>
2023-03-18 20:44:06 -0400

    added math variables

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 62;
+let num2 = 1;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1; 
+let num3 = 12;
+let num4 = 8;
+const mod = num3 % num4;
+const max = Math.max(1, 2, 3, 4, 20);
 // Write your code here
